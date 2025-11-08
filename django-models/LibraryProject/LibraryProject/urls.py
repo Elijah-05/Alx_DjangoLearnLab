b3b8b1d9ea21b1a3e3dc41bf1cf56b416ba3c086
Elijah <ellayab804@gmail.com>
2025-11-08 11:00:18 +0300

    relationship app views make exact as expected

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from relationship_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', views.book_list, name='book-list'),
-    path('library/<int:pk>', views.LibraryDetailView.as_view(), name='library-detail')
+    path('relationship/', include('relationship_app.urls')),
 ]
