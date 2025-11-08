6fd5a3b51ef3db910d51f553724ad9bad85b5a86
Elijah <ellayab804@gmail.com>
2025-11-08 22:35:29 +0300

    user authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', list_books, name='book_list'),  # Function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,8 @@
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 from .models import Library, Book
 
 # Create your views here.
@@ -7,13 +10,15 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# class based view
+def library_detail(request, pk):
+    library = get_object_or_404(Library.objects.prefetch_related('books__author'), pk=pk)
+    return render(request, 'relationship_app/library_detail.html', {'library': library})
 
+# class based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
 
     def get_context_data(self, **kwargs):
         """Injects additional context data specific to the library."""
@@ -21,7 +26,8 @@ class LibraryDetailView(DetailView):
         library = self.get_object()  # Retrieve the current library instance
         context['books'] = library.books.all()  # Add all books in the library to the context
         return context
-    
-def library_detail(request, pk):
-    library = get_object_or_404(Library.objects.prefetch_related('books__author'), pk=pk)
-    return render(request, 'relationship_app/library_detail.html', {'library': library})
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "relationship_app/register.html"
