c7537752272fdedecf12d78979f8da547bb804e1
Elijah <ellayab804@gmail.com>
2025-11-08 11:05:55 +0300

    relationship app views make exact as expected

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Library, Book
 
 # Create your views here.
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
