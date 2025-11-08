14aa0be1f6e95be7c92465abacfcfec5d9f47bbe
Elijah <ellayab804@gmail.com>
2025-11-08 22:46:06 +0300

    user authentication views file update for register

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -28,12 +28,12 @@ class LibraryDetailView(DetailView):
         context['books'] = library.books.all()  # Add all books in the library to the context
         return context
     
+class register(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "relationship_app/register.html"
+    
 class LoginView(CreateView):
     template_name = 'relationship_app/login.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('book_list')
-
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "relationship_app/register.html"
