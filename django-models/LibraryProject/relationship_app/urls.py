191f25a190d7da35e27862f3af25647740c1905d
Elijah <ellayab804@gmail.com>
2025-11-08 22:51:29 +0300

    user authentication views file for logout

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('books/', list_books, name='book_list'),  # Function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
     path('register/', views.register.as_view(), name='register'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('login/', views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
