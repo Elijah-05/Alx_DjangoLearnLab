b9ad311c1b2eae47402c4865b2aa503d88418ca5
Elijah <ellayab804@gmail.com>
2025-11-09 00:00:01 +0300

    authentications and permissions: urls update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     path('admin-dashboard/', views.admin_view, name='admin_view'),
     path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
     path('member-dashboard/', views.member_view, name='member_view'),
+
+    path('add_books/', views.add_book, name='add_book'),
+    path('edit_books/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_books/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
