de33e8e1958b4709fcce4b2b7eff3e1802282d3c
Elijah <ellayab804@gmail.com>
2025-11-14 12:14:11 +0300

    secure redirects and https

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -43,6 +43,19 @@ SESSION_COOKIE_HTTPONLY = True
 
 ALLOWED_HOSTS = []
 
+# Redirect all HTTP requests to HTTPS
+SECURE_SSL_REDIRECT = True   # Must be True in production
+
+# HTTP Strict Transport Security (HSTS)
+SECURE_HSTS_SECONDS = 31536000  # 1 year in seconds
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True  # Allows site to be preloaded in browser HSTS lists
+
+# Secure headers implementation
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = "DENY"
+
 
 # Application definition
 
