83da91f589c1829b750e291160c4cb7decae42af
Elijah <ellayab804@gmail.com>
2025-11-14 10:50:01 +0300

    custom user models in bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractUser
 
 # Create your models here.
 class Book(models.Model):
@@ -9,3 +10,45 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+    
+# Custom user model
+class CustomUser(AbstractUser):
+    date_of_birth: models.DateField
+    profile_photo: models.ImageField
+
+    # objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+    
+# Custom user manager
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            username=username,
+            email=email,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if not extra_fields.get("is_staff"):
+            raise ValueError("Superuser must have is_staff=True")
+        if not extra_fields.get("is_superuser"):
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self.create_user(username, email, password, **extra_fields)
+    
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,49 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager, User
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
-    
-# Custom user manager
-class CustomUserManager(BaseUserManager):
-
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("Users must have an email address")
-
-        email = self.normalize_email(email)
-
-        user = self.model(
-            username=username,
-            email=email,
-            **extra_fields
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-
-        if not extra_fields.get("is_staff"):
-            raise ValueError("Superuser must have is_staff=True")
-        if not extra_fields.get("is_superuser"):
-            raise ValueError("Superuser must have is_superuser=True")
-
-        return self.create_user(username, email, password, **extra_fields)
-    
-# Custom user model
-class CustomUser(AbstractUser):
-    date_of_birth: models.DateField
-    profile_photo: models.ImageField
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.username
 
 
 # Create your models here.
