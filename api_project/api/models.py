b16792d7f9733e4fe5e9a555228a94d1935420cf
Elijah <ellayab804@gmail.com>
2025-11-18 22:27:34 +0300

    feat: project initialization with api and model sample creation

---
+++ api_project/api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
